26b407f64d4b302bb72e4e13f72f419a55c27bca
JairBGarcia <jairbgarcia19@gmail.com>
2024-01-25 19:58:06 -0500

    INICIE EJERICICO

---
+++ Day_1/main.py
@@ -0,0 +1,10 @@
+##-------------------
+##----Ejercicio 1----
+##-------------------
+
+#Impresion en consola
+print("Hola mundo")
+
+#----Datos Primitivos----
+
+## Desarrollado por JAIR ANDRES BERNAL GARCIA - 1098607050
